3bc36399cce6d97c7d8dac3924bba55bb6fb3343
takaisaksen <taka.isaksen@fintechinnovation.no>
2018-11-07 23:20:14 +0100

    working changes to scrape

--- finnestate/spiders/finn_spider.py
+++ finnestate/spiders/finn_spider.py
@@ -9,23 +9,53 @@ class FinnSpider(scrapy.Spider):
     pages_parsed = 1
     entries = 0
 
-    def null_catch(self, function):
-        try:
-            value = function
-            return value
-        except:
-            return None
-
     def parse(self, response):
         main_section = response.css('.main-section')
         result_hit_count = response.xpath('//div[@role="main"]/@data-result-hit-count').extract_first()
+
         print("result hit count: "+result_hit_count)
-        print("Page: "+str(self.pages_parsed))
+        print("Page: "+ str(self.pages_parsed))
+
         for i , entry in enumerate(main_section.css('.result-item')):
             self.entries += 1
-            print("Entry Number: "+str(self.entries))
+            print("Entry Number: "+ str(self.entries))
+
+            # ignore weekly ad med ukens bolig
             if entry.css('span.mrs ::text').extract_first():
-                continue # ignore weekly ad med ukens bolig
+                print("skipped entry due to ad")
+                continue
+
+            # Common Debt and Common Entry - Same line extract
+            common_debt = None
+            common_expenses = None
+            if entry.css('li.hide-lt768 ::text').extract_first():
+                common_debt=entry.css('li.hide-lt768 ::text').extract_first().split(u' \u2022 ')[0]
+                if not len(entry.css('li.hide-lt768 ::text').extract_first().split(u' \u2022 ')) is 1: # check if list is larger than one, second element should be common_expenses
+                    common_expenses = entry.css('li.hide-lt768 ::text').extract_first().split(u' \u2022 ')[1]
+
+            #Owner Building Type and Rooms - Same extract
+            owner = None
+            building_type = None
+            rooms = None
+            if entry.css('li[data-automation-id="bottomRow2"] ::text').extract_first():
+                entry_len = len(entry.css('li[data-automation-id="bottomRow2"] ::text').extract_first())
+                print(entry.css('li[data-automation-id="bottomRow2"] ::text').extract_first().split(u' \u2022 '))
+                if entry_len is 1:
+                    print("in here")
+                    owner = entry.css('li[data-automation-id="bottomRow2"] ::text').extract_first().split(u' \u2022 ')[0];
+                elif entry_len is 2:
+                    owner = entry.css('li[data-automation-id="bottomRow2"] ::text').extract_first().split(u' \u2022 ')[0];
+                    building_type = entry.css('li[data-automation-id="bottomRow2"] ::text').extract_first().split(u' \u2022 ')[1];
+                elif entry_len is 3:
+                    owner = entry.css('li[data-automation-id="bottomRow2"] ::text').extract_first().split(u' \u2022 ')[0];
+                    building_type = entry.css('li[data-automation-id="bottomRow2"] ::text').extract_first().split(u' \u2022 ')[1];
+                    rooms = entry.css('li[data-automation-id="bottomRow2"] ::text').extract_first().split(u' \u2022 ')[2];
+
+            #Price check as sometimes price isnt labeled
+            price = None;
+            if entry.css('span.prm:last-child ::text').extract():
+                price = entry.css('span.prm:last-child ::text').extract()[0];
+
             yield {
                 'id': entry.xpath('//a/@data-finnkode').extract()[i],
                 'href': self.base_url + entry.xpath('//a[@data-search-resultitem=""]/@href').extract()[i],
@@ -33,14 +63,15 @@ class FinnSpider(scrapy.Spider):
                 'real_estate_firm': entry.css('li.truncate ::text').extract_first(),
                 'location': entry.css('div.valign-middle ::text').extract_first(),
                 'square_meter': entry.css('span.prm ::text').extract_first(), #square meter is the first span
-                'price': entry.css('span.prm:last-child ::text').extract()[0], #price is secon span
-                'owner': entry.css('li[data-automation-id="bottomRow2"] ::text').extract_first().split(u' \u2022 ')[0],
-                #'building_type': entry.css('li[data-automation-id="bottomRow2"] ::text').extract_first().split(u' \u2022 ')[1],
-                #'rooms': entry.css('li[data-automation-id="bottomRow2"] ::text').extract_first().split(u' \u2022 ')[2],
+                'price': price, #price is secon span
+                'owner': owner,
+                'building_type': building_type,
+                'rooms': rooms,
                 'ad_title': entry.css('h3.result-item-heading ::text').extract_first(),
-                'common_debt': self.null_catch(entry.css('li.hide-lt768 ::text').extract_first().split(u' \u2022 ')[0]),
-                #'common_expenses': entry.css('li.hide-lt768 ::text').extract_first().split(u' \u2022 ')[1],
+                'common_debt': common_debt,
+                'common_expenses': common_expenses,
                 }
+            print("-"*30);
         
         for next_page in response.css("a.pam").xpath('@href'):
             yield response.follow(next_page, self.parse)
